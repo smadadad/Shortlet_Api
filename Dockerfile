58c2ae2000fb9a0acd84ddc437e62680a3df3590
Adams <animashaunadams@gmail.com>
2024-08-14 22:13:34 +0100

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -7,16 +7,18 @@ WORKDIR /app
 # copies all the files in the current working directory into the container
 COPY . /app/
 
+# tells Flask which file to run by setting an env variable
+ENV FLASK_APP=main.py
+
 # installs flasks inside the container
-RUN pip install --no-cache-dir Flask
+RUN pip install Flask gunicorn
 # RUN apt-get update && apt-get install -y python3 python3-pip
 
 
 # Exposes port 8080 to allow external access to the application 
 EXPOSE 8080
 
-# tells Flask which file to run by setting an env variable
-ENV FLASK_APP=main.py
+
 
 # tells docker to run the application when the conatainer starts
-CMD [ "flask", "run", "--host=0.0.0.0", "--port=8080" ]
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8080", "main:app"]
---
+++ main.tf
@@ -0,0 +1,192 @@
+provider "google" {
+  credentials = file("my-terraform-config/")
+  project     = "time-api-1"
+  region      = "us-central1"
+}
+
+# Google Kubernetes Engine (GKE) cluster
+resource "google_container_cluster" "timeapi_cluster" {
+  name     = "timeapi-cluster"
+  location = "us-central1"
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    service_account = google_service_account.gke_service_account.email
+  }
+}
+
+# Create a service account for GKE
+resource "google_service_account" "gke_service_account" {
+  account_id   = "gke-cluster-sa"
+  display_name = "GKE Cluster Service Account"
+}
+
+# Assign IAM roles to the service account
+resource "google_project_iam_member" "gke_cluster_admin" {
+  project = "time-api-1"
+  role    = "roles/container.clusterAdmin"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
+}
+
+resource "google_project_iam_member" "gke_compute_admin" {
+  project = "time-api-1"
+  role    = "roles/compute.admin"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
+}
+
+resource "google_project_iam_member" "gke_iam_service_account_user" {
+  project = "time-api-1"
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
+}
+
+# Create a VPC
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
+}
+
+# Create a Subnetwork
+resource "google_compute_subnetwork" "timeapisubnet_main" {
+  name          = "timeapisubnet-main"
+  network       = google_compute_network.vpc_network.id
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+}
+
+# Create a Firewall Rule to allow internal/secure communication
+resource "google_compute_firewall" "allow-internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.0.0.0/16"]
+}
+
+# Create NAT Gateway
+resource "google_compute_router" "nat_router" {
+  name    = "nat-router"
+  network = google_compute_network.vpc_network.id
+  region  = "us-central1"
+}
+
+resource "google_compute_router_nat" "nat_gateway" {
+  name                               = "nat-gateway"
+  router                             = google_compute_router.nat_router.name
+  region                             = google_compute_router.nat_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+# Kubernetes Namespace
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "example-namespace"
+  }
+}
+
+# Kubernetes Deployment
+resource "kubernetes_deployment" "timeapi_deployment" {
+  metadata {
+    name      = "timeapi-deployment"
+    namespace = kubernetes_namespace.example.metadata.name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "my-api"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "my-api"
+        }
+      }
+
+      spec {
+        container {
+          image = "gcr.io/${var.project_id}/timeapi:latest"
+          name  = "time_api_container"
+
+          ports {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+# Kubernetes Service
+resource "kubernetes_service" "my_api_service" {
+  metadata {
+    name      = "my-api-service"
+    namespace = kubernetes_namespace.example.metadata.name
+  }
+
+  spec {
+    selector = {
+      app = "my-api"
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
+
+# Kubernetes Ingress
+resource "kubernetes_ingress" "timeapi_ingress" {
+  metadata {
+    name      = "timeapi_ingress"
+    namespace = kubernetes_namespace.example.metadata.name
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          path = "/time"
+          path_type = "prefix"
+          backend {
+            service_name = kubernetes_service.my_api_service.metadata.name
+            service_port = 80
+          }
+        }
+      }
+    }
+
+    tls {
+      secret_name = "timeapi-tls"
+    }
+  }
+}
+
+# Kubernetes TLS Secret 
+resource "kubernetes_secret" "my_api_tls_secret" {
+  metadata {
+    name      = "timeapi-tls"
+    namespace = kubernetes_namespace.example.metadata.name
+  }
+
+  data = {
+    tls.crt = filebase64("${path.module}/certs/tls.crt")
+    tls.key = filebase64("${path.module}/certs/tls.key")
+  }
+
+  type = "kubernetes.io/tls"
+}
