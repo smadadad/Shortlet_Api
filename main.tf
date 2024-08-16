7c927a4783a1d36d9e561a8887ad59cce7279f6b
Adams <138290364+smadadad@users.noreply.github.com>
2024-08-16 15:51:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,27 @@
 # Google Provider Configuration
 provider "google" {
   credentials = var.gcp_credentials
-  project     = var.project_id
+    project     = var.project_id
   region      = "us-central1"
 }
 
 # Create a service account for GKE
-resource "google_service_account" "gke_service_account" {
-  account_id   = "gke-cluster-sa"
-  display_name = "GKE Cluster Service Account"
-}
+#resource "google_service_account" "gke_service_account" {
+#  account_id   = "gke-cluster-sa"
+#  display_name = "GKE Cluster Service Account"
+#}
 
 
 # Google Kubernetes Engine (GKE) cluster
-resource "google_container_cluster" "timeapi_cluster1" {
-  name     = "timeapi-cluster1"
-  location = "us-central1"
+#resource "google_container_cluster" "timeapi_cluster1" {
+ # name     = "timeapi-cluster1"
+  #location = "us-central1"
 
   initial_node_count = 1
   node_config {
     machine_type = "e2-medium"
 
-    disk_size_gb = 75 
+    disk_size_gb = 75 # This will set to 10 instead of 100
     service_account = google_service_account.gke_service_account.email
   }
 }
@@ -47,42 +47,23 @@ resource "google_project_iam_member" "gke_iam_service_account_user" {
   member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
-
-
-# Data source to check for an existing VPC network
-data "google_compute_network" "existing_vpc_network" {
-  name    = "vpc-network"
-  project  = var.project_id
-}
-
-# Create a VPC network only if it doesn't exist
+# Create a VPC
 resource "google_compute_network" "vpc_network" {
-  count = length(data.google_compute_network.existing_vpc_network.id) == 0 ? 1 : 0
-  name  = "vpc-network"
+  name = "vpc-network"
 }
 
-
-# Check if the Subnetwork already exists
-data "google_compute_subnetwork" "existing_subnet" {
-  name    = "timeapisubnet"
-  region  = "us-central1"
-  project = var.project_id
-}
-# Create a Subnetwork 
+# Create a Subnetwork
 resource "google_compute_subnetwork" "timeapisubnet" {
-  count         = length(data.google_compute_subnetwork.existing_subnet.id) == 0 ? 1 : 0
   name          = "timeapisubnet"
-  network       = google_compute_network.vpc_network[0].id
+  network       = google_compute_network.vpc_network.id
   ip_cidr_range = "10.0.0.0/16"
   region        = "us-central1"
 }
 
-
-
-# Create a Firewall Rule 
+# Create a Firewall Rule to allow internal/secure communication
 resource "google_compute_firewall" "allow-internal" {
   name    = "allow-internal"
-  network = google_compute_network.vpc_network[0].id
+  network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
@@ -92,44 +73,20 @@ resource "google_compute_firewall" "allow-internal" {
   source_ranges = ["10.0.0.0/16"]
 }
 
-
-
-# Create a NAT Router only if it doesn't exist
+# Create NAT Gateway
 resource "google_compute_router" "nat_router" {
   name    = "nat-router"
-  network = google_compute_network.vpc_network[0].id
+  network = google_compute_network.vpc_network.id
   region  = "us-central1"
 }
 
-# check for an existing NAT Router
-data "google_compute_router" "existing_nat_router" {
-  name    = "nat-router"
-  region  = "us-central1"
-  project = var.project_id
-  network = google_compute_network.vpc_network[0].id
-}
-
-# Check if the NAT Gateway already exists
-data "google_compute_router_nat" "existing_nat_gateway" {
-  name   = "nat-gateway"
-  region = "us-central1"
-  router = data.google_compute_router.existing_nat_router.name
-  project = var.project_id
-}
-
-
-# Create a NAT Gateway , only if the VPC network is created
 resource "google_compute_router_nat" "nat_gateway" {
-  count = length(data.google_compute_router_nat.existing_nat_gateway.id) == 0 ? 1 : 0
-  name = "nat-gateway"
-  router = google_compute_router.nat_router.name
-  region  = google_compute_router.nat_router.region
-  nat_ip_allocate_option = "AUTO_ONLY"
+  name                               = "nat-gateway"
+  router                             = google_compute_router.nat_router.name
+  region                             = google_compute_router.nat_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
-
-
-
 
 # Kubernetes Namespace
 resource "kubernetes_namespace" "timeapi_ns" {
@@ -165,10 +122,6 @@ resource "kubernetes_deployment" "timeapi_deployment" {
         container {
           image = "gcr.io/${var.project_id}/timeapi:latest"
           name  = "time_api_container"
-          port{
-            container_port = 8080
-
-          }
 
           }
         }
