49ed8de05cab0dc43e2865430b36e5515e7dfe48
Adams <138290364+smadadad@users.noreply.github.com>
2024-08-17 00:14:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "google" {
   region      = "us-central1"
 }
 
+
 # Create a service account for GKE
 resource "google_service_account" "gke_service_account" {
   account_id   = "gke-cluster-sa"
