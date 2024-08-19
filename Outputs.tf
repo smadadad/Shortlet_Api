ff517cb51f44e683bbc95f99ff2403ba528f0f23
Adams <138290364+smadadad@users.noreply.github.com>
2024-08-19 09:55:36 +0100

    Update Outputs.tf

--- Outputs.tf
+++ Outputs.tf
@@ -5,3 +5,7 @@ output "kubernetes_cluster_name" {
 output "cluster_ca_certificate" {
   value = google_container_cluster.timeapi_cluster1.master_auth[0].cluster_ca_certificate  # Update the resource name
 }
+output "gke_service_account_private_key" {
+  value = google_service_account_key.gke_sa_key.private_key
+  sensitive = true
+}
